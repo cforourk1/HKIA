3c295fbd7db6fd7ec23ac4004fbd325b58a4ea11
Crystal <cforourk@umich.edu>
2026-03-29 12:13:41 -0400

    function is working as intendend to sort on region and rarity

--- seasideFunctionsUI.js
+++ seasideFunctionsUI.js
@@ -1,3 +1,11 @@
+/**
+ * This returns fish by region and rarity
+ * @param {string} region - filter by region
+ * @param {string}  rarity - filter by rarity
+ * @returns {string} lists of fish that match parameters
+ */
 function sortRarity(region, rarity) {
-    const filterRarity = seasideResortFish.filter()
+    const filterRarity = seasideResortFish.filter(fish => rarity === fish.rarity && region === fish.region)
+    console.log(filterRarity, region)
+    return filterRarity
 }
