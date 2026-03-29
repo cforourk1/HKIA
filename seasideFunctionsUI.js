d96bbd0f6f87be87bf8682afd79c96c4b4249e21
Crystal <cforourk@umich.edu>
2026-03-29 12:15:49 -0400

    added another comment

--- seasideFunctionsUI.js
+++ seasideFunctionsUI.js
@@ -5,6 +5,8 @@
  * @returns {string} lists of fish that match parameters
  */
 function sortRarity(region, rarity) {
+/*I am using a filter to go through the array and sorty the fish by rarity and ragion and return values based on user input. Ther could be mutiple filters selected.
+*/
     const filterRarity = seasideResortFish.filter(fish => rarity === fish.rarity && region === fish.region)
     console.log(filterRarity, region)
     return filterRarity
